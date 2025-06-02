a8b0037f7d1acb2d470e5f3d82e26f3686bcc507
manoj1619git config --global user.email manoj.ummidi@softdel.comgit config --global user.name manoj1619 <manoj.ummidi@softdel.com>
2025-06-02 15:18:06 +0530

    TC4

--- src/indent_space.c
+++ src/indent_space.c
@@ -8,9 +8,7 @@
  * @param b Second integer
  * @return Sum of a and b
  */
-int add(int a, int b) {
-    return a + b;
-}
+int add(int a, int b) { return a + b; }
 
 /**
  * @brief Main program entry point.
@@ -18,13 +16,12 @@ int add(int a, int b) {
  * @return int Returns the sum of two integers.
  */
 int main() {
-    int x = 10;
-    int y = 20;
+  int x = 10;
+  int y = 20;
 
-    if (x != 5) {  // Changed condition to avoid always true warning
-        printf("X is not 5\n");
-    }
+  if (x > 5) { // Changed condition to avoid always true warning
+    printf("X is greater than 5\n");
+  }
 
-    return add(x, y);
+  return add(x, y);
 }
-
