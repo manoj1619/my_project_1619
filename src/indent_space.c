d634002d0f3cd693e85008ec55d1b236dd9a799d
manoj1619git config --global user.email manoj.ummidi@softdel.comgit config --global user.name manoj1619 <manoj.ummidi@softdel.com>
2025-06-02 16:09:50 +0530

    TC4

--- src/indent_space.c
+++ src/indent_space.c
@@ -1,30 +1,3 @@
-#include <stdio.h>
-
-#define MAXVAL 100
-
-/**
- * @brief Adds two integers.
- * @param a First integer
- * @param b Second integer
- * @return Sum of a and b
- */
-int add(int a, int b) { return a + b; }
-
-/**
- * @brief Main program entry point.
- *
- * @return int Returns the sum of two integers.
- */
-int main() {
-  int x = 10;
-  int y = 20;
-
-  if (x > 5) { // Changed condition to avoid always true warning
-    printf("X is greater than 5\n");
-  }
-
-
-
-
-  return add(x, y);
+#include <stdio.h>int main()
+{    printf("Bad indentation\n");return 0;
 }
