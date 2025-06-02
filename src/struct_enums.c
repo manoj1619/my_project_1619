d3fc35cad07bf427a11dac23290dc16ea960cd3b
manoj1619git config --global user.email manoj.ummidi@softdel.comgit config --global user.name manoj1619 <manoj.ummidi@softdel.com>
2025-06-02 11:19:37 +0530

    TC1

--- src/struct_enums.c
+++ src/struct_enums.c
@@ -1,9 +1,9 @@
 // ❌ BAD: typedef name not following s_MyStruct or e_MyEnum convention
-typedef struct {
-  int value;
-} mystruct_t;
+//typedef struct {
+//  int value;
+//} mystruct_t;
 
-typedef enum { RED, GREEN, BLUE } myenum_t;
+//typedef enum { RED, GREEN, BLUE } myenum_t;
 
 // ✅ GOOD:
 typedef struct {
